b8d9b040216d1f476ed1fe39259af32bdf2edef5
haojun-mah <mahhaojun03@gmail.com>
2025-10-17 16:02:44 +0800

    fix: apify client init

--- apify_client.py
+++ apify_client.py
@@ -1,35 +1,6 @@
 from apify_client import ApifyClient
-from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
 
-config = dotenv_values(".env")
-api_token = config.get("APIFY_TOKEN")
-if not api_token:
-    raise ValueError("APIFY_TOKEN is missing from .env")
-
-client = ApifyClient(api_token)
-
-# Prepare the Actor input
-run_input = {
-    "hashtags": ["fyp"],
-    "resultsPerPage": 100,
-    "profileScrapeSections": ["videos"],
-    "profileSorting": "latest",
-    "excludePinnedPosts": False,
-    "searchSection": "",
-    "maxProfilesPerQuery": 10,
-    "scrapeRelatedVideos": False,
-    "shouldDownloadVideos": False,
-    "shouldDownloadCovers": False,
-    "shouldDownloadSubtitles": False,
-    "shouldDownloadSlideshowImages": False,
-    "shouldDownloadAvatars": False,
-    "shouldDownloadMusicCovers": False,
-    "proxyCountryCode": "None",
-}
-
-# Run the Actor and wait for it to finish
-run = client.actor("GdWCkxBtKWOsKjdch").call(run_input=run_input)
-
-# Fetch and print Actor results from the run's dataset (if there are any)
-for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-    print(item)
+load_dotenv()
+client = ApifyClient(os.environ["APIFY_TOKEN"])
